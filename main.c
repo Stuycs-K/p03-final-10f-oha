3e5c7f165b339eb4e8d42a18a951a018d133329f
AdamOh2 <adm2k8@gmail.com>
2026-01-07 02:50:35 -0500

    opens file

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include "editor.h"
 int main(int argc, char *argv[]){
   char * filename = argv[1];
-  int openfile = open(filename, O_RDWR | O_CREAT, 0666);
+  int openfile = open(filename, O_RDWR, 0666);
   if(openfile < 0){
     err();
-  )
-  struct stat
-  stat(filename, )
-  int readfile = read(openfile, )
+  }
+  struct stat *statbuff = malloc(sizeof(struct stat));
+  stat(filename, statbuff);
+  char * buff = malloc(statbuff->st_size);
+  int readfile = read(openfile, buff, sizeof(buff));
+  if (readfile < 0){
+    err();
+  }
+  initscr();
+  printw("%s", buff);
+  refresh();
+  getch();
+  endwin();
 }
