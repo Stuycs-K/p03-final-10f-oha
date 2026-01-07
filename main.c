6a1936b2adabdd0e8a89a005130daba7387619a1
AdamOh2 <adm2k8@gmail.com>
2026-01-07 03:05:54 -0500

    fixed bug

--- main.c
+++ main.c
@@ -8,12 +8,42 @@ int main(int argc, char *argv[]){
   struct stat *statbuff = malloc(sizeof(struct stat));
   stat(filename, statbuff);
   char * buff = malloc(statbuff->st_size);
-  int readfile = read(openfile, buff, sizeof(buff));
+  int readfile = read(openfile, buff, statbuff->st_size);
+  buff[readfile] = '\0';
   if (readfile < 0){
     err();
   }
+  printf("%s",buff);
   initscr();
-  printw("%s", buff);
+  clear();
+  scrollok(stdscr, TRUE);
+
+  int row = 0;
+  int col = 0;
+  int maxy, maxx;
+  getmaxyx(stdscr, maxy, maxx);
+
+  for (int i = 0; i < statbuff->st_size; i++){
+    char n = buff[i];
+
+    if(n == '\n'){
+      row++;
+      col = 0;
+    }
+    else{
+      mvaddch(row, col, n);
+      col++;
+      if(col >= maxx){
+        row++;
+        col = 0;
+      }
+    }
+
+    if(row>=maxy){
+      scroll(stdscr);
+      row = maxy - 1;
+    }
+  }
   refresh();
   getch();
   endwin();
